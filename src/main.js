4fcf5dc42d2e7f0bb637b628eff9c295bf332170
Boris Vergasov <starsfer839@yandex.ru>
2025-07-20 21:40:46 +0300

    удалено лишнее поле в top products

--- src/main.js
+++ src/main.js
@@ -122,7 +122,7 @@ sellerStats.forEach((seller, index) => {
     .sort((a, b) => b[1] - a[1]))
     .slice(0, 10)
     .map(([name, value]) =>
-      ({ [name]: value,
+      ({
          "quantity": value,
          "sku": name,
        })
